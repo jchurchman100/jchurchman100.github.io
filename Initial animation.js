3683108c49020f23067aa5e039f49b29b8997ac5
Jared <jchurchman@utexas.edu>
2020-03-06 04:07:22 -0600

    hi

--- Initial animation.js
+++ Initial animation.js
@@ -1071,8 +1071,8 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 		}
 		stage.enableMouseOver(20);
 		
-		document.getElementsByClassName('navbar-brand').addEventListener('mouseover', over.bind(this));
-		document.getElementsByClassName('navbar-brand').addEventListener('mouseout', out.bind(this));
+		document.getElementById('sig').addEventListener('mouseover', over.bind(this));
+		document.getElementById('sig').addEventListener('mouseout', out.bind(this));
 
 		// this.logo_mc.addEventListener('mouseover',over.bind(this));
 		// this.logo_mc.addEventListener('mouseout',out.bind(this));
